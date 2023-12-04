a519467bcfd2f15db74859494ab2e15ed9ffdb5f
Yago Inacio <yagofaran@gmail.com>
2023-12-04 12:54:28 -0300

    created project repo interface

---
+++ internal/projects/adapters/persistance/in_memory/in_memory_projects_repository.go
@@ -0,0 +1,61 @@
+package repositories
+
+import (
+	"errors"
+
+	domain "github.com/yagoinacio/portfolio-server/internal/projects/domain/entities"
+)
+
+type InMemoryProjectsRepository struct{}
+
+func NewInMemoryProjectsRepository() *InMemoryProjectsRepository {
+	return &InMemoryProjectsRepository{}
+}
+
+var Projects []domain.Project
+
+func (repo *InMemoryProjectsRepository) Save(project *domain.Project) error {
+	Projects = append(Projects, *project)
+
+	return nil
+}
+
+func (r *InMemoryProjectsRepository) FindDisplayable() ([]domain.Project, error) {
+	var displayable []domain.Project
+
+	for _, project := range Projects {
+		if project.Display {
+			displayable = append(displayable, project)
+		}
+	}
+
+	return displayable, nil
+}
+
+func (repo *InMemoryProjectsRepository) FindById(id string) (domain.Project, error) {
+	var project *domain.Project
+
+	for _, proj := range Projects {
+		if proj.ID.Hex() == id {
+			project = &proj
+			break
+		}
+	}
+
+	if project == nil {
+		return domain.Project{}, errors.New("experience not found")
+	}
+
+	return *project, nil
+}
+
+func (repo *InMemoryProjectsRepository) Update(project *domain.Project) error {
+	for i, proj := range Projects {
+		if proj.ID.Hex() == project.ID.Hex() {
+			Projects[i] = *project
+			break
+		}
+	}
+
+	return nil
+}
---
+++ internal/projects/domain/repositories/projects_repository_interface.go
@@ -0,0 +1,10 @@
+package repositories
+
+import domain "github.com/yagoinacio/portfolio-server/internal/projects/domain/entities"
+
+type ProjectsRepositoryInterface interface {
+	Save(project *domain.Project) error
+	listDisplayable() ([]*domain.Project, error)
+	FindById(id string) (domain.Project, error)
+	Update(experience *domain.Project) error
+}
